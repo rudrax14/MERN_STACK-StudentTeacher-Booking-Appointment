27130eb7d9a57eb32d2927c6f0b3547e08d8a431
rudrax14 <m.rudra2003@gmail.com>
2024-03-06 21:45:17 +0530

    Update backend configuration and file names

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,10 @@ mongoose
 
 const port = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Tutor-Time API!')
+})
+
 app.listen(port, () => {
   console.log('App listening on port ' + port)
 })
