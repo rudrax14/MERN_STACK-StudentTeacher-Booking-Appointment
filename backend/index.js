0a7714aff004c42283087cdef87e754fb60e0592
rudrax14 <m.rudra2003@gmail.com>
2024-03-09 15:33:53 +0530

    Refactor backend index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,48 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./.env" });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-dotenv.config({ path: './.env' });
-
-
-
-
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
-    console.log('DB Connected');
-  }).catch((err) => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
     console.log(err);
   });
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Tutor-Time API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Tutor-Time API!");
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const adminRoutes = require("./routes/adminRoutes");
+const teacherRoutes = require("./routes/teacherRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
-const adminRoutes = require('./routes/adminRoutes');
-const teacherRoutes = require('./routes/teacherRoutes');
-const studentRoutes = require('./routes/studentRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-
-app.use('/api/v1/admin', adminRoutes);
-app.use('/api/v1/teachers', teacherRoutes);
-app.use('/api/v1/student', studentRoutes);
-app.use('/api/v1/messages', messageRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/teachers", teacherRoutes);
+app.use("/api/v1/student", studentRoutes);
+app.use("/api/v1/messages", messageRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 app.listen(port, () => {
-  console.log('App listening on port ' + port);
+  console.log("App listening on port " + port);
 });
 
 module.exports = app;
