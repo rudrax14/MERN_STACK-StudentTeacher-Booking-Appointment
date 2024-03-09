c5afb0df2b8b009a69faab21ea9e2de15fdec9ee
rudrax14 <m.rudra2003@gmail.com>
2024-03-09 18:12:57 +0530

    Refactor studentRoutes.js to use consistent spacing and add welcome route

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,14 +1,17 @@
-const express=require('express');
+const express = require('express');
 const { login, verifyToken } = require('../controllers/authController');
 const { register, bookAppointment, getTeacherWithAppointments, registeredAppointments } = require('../controllers/studentController');
 const { allow } = require('../controllers/adminController');
 const router = express.Router();
 
+router.get('/get', (req, res) => {
+    res.send("Welcome to the student Tutor-Time API!")
+})
 router.route('/register').post(register);
 router.route('/login').post(login)
-router.route('/appointment/:id').patch(verifyToken,allow('student'),bookAppointment)
-router.route('/appointment/getTeachersWithAppointments').get(verifyToken,allow('student'),getTeacherWithAppointments)
-router.route('/appointment/getRegisteredAppointments').get(verifyToken,allow('student'),registeredAppointments)
+router.route('/appointment/:id').patch(verifyToken, allow('student'), bookAppointment)
+router.route('/appointment/getTeachersWithAppointments').get(verifyToken, allow('student'), getTeacherWithAppointments)
+router.route('/appointment/getRegisteredAppointments').get(verifyToken, allow('student'), registeredAppointments)
 
 
 module.exports = router
